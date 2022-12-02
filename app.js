57bf1d63cb9f8df964aefa6aeb28eb46258b1618
pausiroy <pausiroy@gmail.com>
2022-12-02 12:40:05 -0800

    Create success and error functions to check input validity

--- app.js
+++ app.js
@@ -11,12 +11,38 @@ const password2 = document.getElementById("password2");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    checkInput();
-})
+    checkInputs();
+});
 
 // function to check if inputs are valid 
 
-function checkInput() {
- 
+function checkInputs() {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
 
+  if(usernameValue === "") {
+    setErrorFor(username, "Username cannot be blank");
+  } else {
+    setSuccessFor(username);
+  }
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+
+    formControl.className = "form-control error"
+    
+    small.innerText = message
+
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+
+    formControl.className = "form-control success"   
 }
